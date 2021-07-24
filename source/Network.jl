a6ac63d7e72b4ac4aa049364309be0d3f0c1ddcd
Lauri Himanen <lauri.himanen@gmail.com>
2021-07-24 18:28:20 +0300

    Added global list of nodes and a method for getting random node.

--- source/Network.jl
+++ source/Network.jl
@@ -1,27 +1,28 @@
 module Network
 
 include("./Ledger.jl")
-using .Ledger: Transaction
+using .Ledger: Transaction, Blockchain
 
 mutable struct Node
     pool::Dict{String,Transaction}
-    blockchain::Blockchain
-    Node() = new(Dict{String,Transaction}(), Blockchain())
+    #blockchain::Blockchain
+    #Node() = new(Dict{String,Transaction}(), Blockchain())
+    Node() = new(Dict{String,Transaction}())
 end
 
-nodes::Vector{Node}
+# Initialize a fixed number of nodes
+nodes = [Node() for i in 1:10]
 
-function getNTransactions(pool::Pool, n::UInt)
-    sorted = sort(pool.transactions, by= x -> x.fee, rev=true)
-    return sorted
-    # return sorted[0 min(n, size(sorted, 0)]
+function getNTransactions(node::Node, n::UInt)
+    sorted = sort(node.pool, by= x -> x.fee, rev=true)
+    return sorted[1:min(n, size(sorted, 1))]
 end
 
-function getRandomNode(nodes::Vector{Node})
-    return nodes[rand(1:size(nodes, 0))]
+function getRandomNode()
+    return nodes[rand(1:size(nodes, 1))]
 end
 
-function addTransaction(tx::Transaction)
+function addTransaction(node::Node, tx::Transaction)
     node.pool[tx.signature] = tx
 end
 
@@ -33,7 +34,7 @@ function mine()
     while true
         print("mine")
         # Get transactions to verify
-        transactions = getNTransactions(node, 1)
+        # transactions = getNTransactions(node, 1)
 
         # Get the previous block hash
         # prev_block = getPreviousBlock(node)
@@ -45,13 +46,16 @@ function mine()
 end
 
 function transact()
+    # Select a random Node for inserting new transactions
+    node = getRandomNode()
+
     while true
         print("buy")
         # Create new transaction
         tx = Transaction(0, "aki", 0, "marc", 0, "hi", "kjehkjeh")
 
         # Add transaction to pool
-        addTransaction(tx)
+        addTransaction(node, tx)
 
         # Wait for transaction to complete
     end
